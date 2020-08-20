957bb47b0e654b92c41f86ac082f3cf4f8b23847
joyui <seclusionor@gmail.com>
2020-08-20 11:18:11 +0900

    1~5 완료

--- VCANUS_Test/Q5.py
+++ VCANUS_Test/Q5.py
@@ -1,19 +1,35 @@
+
+import copy
+
 with open('VCANUS_Test\\resource\\pond.txt', mode='r', encoding='utf-8') as f:
     pond = []
     for line in f.readlines():
         pond.append(line.rstrip().split(" "))
     
+for i in range(len(pond)):
+    for j in range(len(pond[i])):
+        pond[i][j] = int(pond[i][j])
 
-print(pond[0])
-print(pond[1])
-print(pond[2])
-print(pond[3])
-print(pond[4])
-print(pond[5])
-print(pond[6])
-print(pond[7])
-print(pond[8])
-print(pond[9])
 
-for i in pond:
-    for
+
+#
+depth = 0
+while True:
+    tmp = copy.deepcopy(pond)
+    for i in range(1,len(pond)-1):
+        for j in range(1,len(pond[i])-1):
+            if pond[i][j] > 0:
+                if (pond[i][j] <= pond[i-1][j]) and (pond[i][j] <= pond[i][j-1]) and (pond[i][j] <= pond[i][j+1]) and (pond[i][j] <= pond[i+1][j]):
+                    pond[i][j] = pond[i][j] +1
+    if tmp == pond:
+        for i in range(len(pond)):
+            for j in range(len(pond[i])):
+                depth += pond[i][j]
+        break
+
+
+for i in range(len(pond)):
+    for j in range(len(pond[i])):
+       print(pond[i][j],end=" ")
+    print()
+print(depth)
